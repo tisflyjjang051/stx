07f3f1635eb6dc42db557517a2799c4a1af5a811
jang sung ho <tisflyjjang051@gmail.com>
2023-01-18 10:30:18 +0900

    자동 기능 추가

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
 Splitting();
 //x,y,z
 //transfotm:translateX()
-gsap.from(".main-visual__txt .char", { opacity: 0, y: 100, duration: 1, stagger: 0.02 });
+
+const btnAuto = document.querySelector(".btn--auto");
+btnAuto.addEventListener("click", function () {
+  if (mainVisualSwiper.autoplay.paused) {
+    mainVisualSwiper.autoplay.run();
+    btnAuto.classList.remove("off");
+  } else {
+    mainVisualSwiper.autoplay.pause();
+    btnAuto.classList.add("off");
+  }
+});
+
+gsap.from(".main-visual__txt .char", { opacity: 0, y: 100, duration: 1, stagger: 0.02, ease: "power4" });
+const mainVisualSwiper = new Swiper(".main-visual", {
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 2000,
+  },
+  pagination: {
+    el: ".main-visual .pagination .pagination-box",
+    type: "bullets",
+  },
+});
+console.log(mainVisualSwiper.autoplay.paused);
